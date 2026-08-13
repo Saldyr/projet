ebbf7b4100c4a76687ae1a6691e50bdfb1c65dba
Saldyr <alex.franco12836@gmail.com>
2026-08-13 21:57:55 +0200

    feat: add frontend from alternance repo (gp_devclassico/sportify/front)

---
+++ front/src/pages/profil/ProfilDetailPage.tsx
@@ -0,0 +1,8 @@
+export default function ProfilDetailPage(){
+
+    return(
+        <>
+            ProfilDetailPage
+        </>
+    )
+}
---
+++ front/src/pages/profil/ProfilEditPage.tsx
@@ -0,0 +1,8 @@
+
+
+export default function ProfilEditPage(){
+
+    return (
+        <>Profil Edit Page</>
+    )
+}
